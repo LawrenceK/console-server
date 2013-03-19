1fff74f9fcf0d78ca3d9b3850d941e5b8206e10a
Lawrence Klyne <githubatlklyne.co.uk>
2013-03-19 13:51:35 +0000

    Bump to 0.2, should now cleanly install from pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 setup(
     name="ConsoleServer",
-    version="0.1",
+    version="0.2",
     description="This is a logging console server that can also accessed over ssh.",
     packages=find_packages(),
     scripts=[],
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=['twisted>=12.2', 'pyserial>=2.5', 'configobj>=4.7'],
+    install_requires=[
+        'twisted>=12.2',
+        'pyserial>=2.5',
+        'configobj>=4.7',
+        'pycrypto',
+        'pyasn1',
+    ],
 
     package_data={
         '': ['*.ini'],
