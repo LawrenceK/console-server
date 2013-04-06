7ec44566f5edaef9ed01ea7dd8c9a3cbed5d9e41
Lawrence Klyne <githubatlklyne.co.uk>
2013-04-06 20:00:03 +0100

    Move import so logging configured first

--- consoleserver/main.py
+++ consoleserver/main.py
@@ -3,12 +3,11 @@ _log = logging.getLogger(__name__)
 
 from twisted.internet import reactor
 
-from consoleserver import console_server
-
 
 def main():
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s %(levelname)s [%(funcName)s] %(message)s')
+    from consoleserver import console_server
     console_server()
     _log.info("Starting twisted reactor")
     reactor.run()
